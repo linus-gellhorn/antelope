7fa9901eb928ced4c799670030db5c50b28133cc
Linus Gellhorn <linus.g@hotmail.com>
2022-03-09 17:16:56 +0000

    Added example endpoints for experimenting

--- app.rb
+++ app.rb
@@ -3,3 +3,22 @@ require 'sinatra'
 get '/' do
   'Hello world!'
 end
+
+get '/test' do
+  'Testing, testing, 123.'
+end
+
+get '/hello/:name' do
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params['name'] is 'foo' or 'bar'
+  "Hello #{params['name']}!"
+end
+
+get '/posts' do
+  # matches "GET /posts?title=foo&author=bar"
+  title = params['title']
+  author = params['author']
+  # uses title and author variables; query is optional to the /posts route
+  author
+  title
+end
